27d62c5ad868136797de355b002a2b7f805d33d4
wangyungang <wangyungang@huya.com>
2018-10-16 16:17:23 +0800

    modify rs

--- MultipathTCP-Swift/MultipathTCP-Swift/ViewController.swift
+++ MultipathTCP-Swift/MultipathTCP-Swift/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func realSendReq() {
+    }
 }
 
