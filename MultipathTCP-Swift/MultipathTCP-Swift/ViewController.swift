9c0baa1b97436f5c5933d1d391180a4e081d5f08
wangyungang <wangyungang@huya.com>
2018-10-16 15:51:58 +0800

    add swift proj

---
+++ MultipathTCP-Swift/MultipathTCP-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipathTCP-Swift
+//
+//  Created by 王云刚 on 2018/10/16.
+//  Copyright © 2018年 王云刚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MultipathTCP-Swift/MultipathTCP-SwiftTests/MultipathTCP_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  MultipathTCP_SwiftTests.swift
+//  MultipathTCP-SwiftTests
+//
+//  Created by 王云刚 on 2018/10/16.
+//  Copyright © 2018年 王云刚. All rights reserved.
+//
+
+import XCTest
+@testable import MultipathTCP_Swift
+
+class MultipathTCP_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
