129ecc4000cad3daf65bb883c98ac49e548e6539
wangyungang <wangyungang@huya.com>
2018-10-16 16:27:35 +0800

    modify rs

--- MultipathTCP-Swift/MultipathTCP-Swift/ViewController.swift
+++ MultipathTCP-Swift/MultipathTCP-Swift/ViewController.swift
@@ -15,7 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    /**
+    Simple example from : http://blog.multipath-tcp.org/blog/html/2017/07/05/mptcp_experiments_on_ios_11_beta.html
+    */
     func realSendReq() {
+        let config = URLSessionConfiguration.ephemeral
+        config.multipathServiceType = URLSessionConfiguration.MultipathServiceType.handover
+        let session = URLSession(configuration: config)
+//        let url = URL(string: "http://multipath-tcp.org/data/uml/vmlinux_64")
+//        let task = session.dataTask(with: url!, completionHandler:{
+//            
+//        })
+//        task.resume()
     }
 }
 
